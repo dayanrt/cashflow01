e7c9c2182f6362916aa605152fcfcc0c43b5d7b6
dayanrt <158417567+dayanrt@users.noreply.github.com>
2024-02-01 05:55:44 -0600

    Add files via upload

--- Dayan01.py
+++ Dayan01.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pip
 pip.main(["install", "openpyxl"])
 
-st.title ('Aprende a usar tu base de datos')
+st.title ('First tests for cashflow')
 
 df = pd.read_excel('Cashflow02.xlsx')
 
