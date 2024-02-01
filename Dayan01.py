9b3d8ae92b4f8ff89a98f3e52d66c5a0022da12d
dayanrt <158417567+dayanrt@users.noreply.github.com>
2024-02-01 11:26:32 -0600

    Add files via upload

--- Dayan01.py
+++ Dayan01.py
@@ -4,12 +4,7 @@ import pandas as pd
 
 st.title ('Cashflow Analysis')
 
-df = pd.read_excel(
-    io='Cashflow02.xlsx',
-sheet_name='GeneralPipelineProgress',
-usecols='A:S',
-nrows=1000,
-)
+
 
 
 st.write(df)
