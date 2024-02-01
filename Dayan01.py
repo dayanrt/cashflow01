edc011ffc4f4df11b5c6199fcbeefff0619b1c9c
dayanrt <158417567+dayanrt@users.noreply.github.com>
2024-02-01 11:22:50 -0600

    Add files via upload

--- Dayan01.py
+++ Dayan01.py
@@ -2,15 +2,14 @@ import streamlit as st
 import pandas as pd
 
 
-st.title ('First tests for cashflow')
+st.title ('Cashflow Analysis')
 
 df = pd.read_excel(
     io='Cashflow02.xlsx',
-engine='openpyxl',
 sheet_name='GeneralPipelineProgress',
 usecols='A:S',
 nrows=1000,
 )
 
 
-st.dataframe(df)
+st.write(df)
